a8acd0705e719b35064236d149fe6d2ba145f566
kangabell <k@kangabell.co>
2013-08-21 10:28:51 -0400

    Cleaned things up to be more wordpress.org compliant and removed some extraneous junk.

--- index.php
+++ index.php
@@ -1,42 +1,17 @@
-<?php get_header(); ?>
-			
-			<div id="content" class="wrap clearfix">
-			
-				<div id="main" class="eightcol first clearfix" role="main">
-					<?php query_posts("showposts=1") ?>
-				    <?php the_post(); ?>
-					    
-						<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-							<header class="article-header">
-							
-								<p class="vcard"><?php
-								  printf(__('<time datetime="%1$s" pubdate>%2$s</time>', 'serena'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')) );
-								?></p>
-								<h1 class="entry-title" itemprop="headline"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-								<p class="author"><?php
-								  printf(__('by %1$s', 'serena'), serena_get_the_author_posts_link());
-								?></p>
-							    
-						
-						    </header> <!-- end article header -->
-							<section class="entry-content clearfix">
-								<?php the_content(); ?>
-						    </section> <!-- end article section -->
-						
-						    <footer class="article-footer">
-								<?php wp_link_pages(); ?>
-						    </footer> <!-- end article footer -->
-							
-						</article> <!-- end article -->
-							
-					<?php wp_reset_query(); ?>
-				</div> <!-- end main -->
-									
-			    <?php 
-				get_template_part( 'sidebar_home' );
-				?>
-				
-    
-			</div> <!-- end #content -->
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-<?php get_footer(); ?>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require('./wp-blog-header.php');
--- sidebar_page.php
+++
@@ -1,5 +0,0 @@
-				<div id="sidebar_page" class="sidebar fourcol last clearfix" role="complementary">
-
-					<?php dynamic_sidebar( 'sidebar_page' ); ?>
-
-				</div>
