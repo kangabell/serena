f22ba08191b880e0107d95ef5b6a2e9c85fbe3a3
kangabell <k@kangabell.co>
2020-10-06 15:19:06 -0400

    fix indentation

--- index.php
+++ index.php
@@ -1,43 +1,43 @@
 <?php get_header(); ?>
 			
-			<div id="content" class="wrap clearfix">
+	<div id="content" class="wrap clearfix">
+
+		<div id="main" class="eightcol first clearfix" role="main">
+							
+			<?php
+			if (have_posts()) : while (have_posts()) : the_post();
+
+				get_template_part( 'template-parts/content' );
+
+			endwhile;
+			?>	
+				
+				<nav class="wp-prev-next">
+					<ul>
+						<li class="prev-link"><?php next_posts_link(__('&laquo; Older', "serena")) ?></li>
+						<li class="next-link"><?php previous_posts_link(__('Newer &raquo;', "serena")) ?></li>
+					</ul>
+				</nav>					
+
+			<?php else : ?>
 			
-			    <div id="main" class="eightcol first clearfix" role="main">
-									
-					<?php
-					if (have_posts()) : while (have_posts()) : the_post();
+				<article id="post-not-found" class="hentry clearfix">
+					<header class="article-header">
+						<h2 class="h1"><?php _e("Article Missing", "serena"); ?></h2>
+					</header>
+					<section class="entry-content">
+						<p><?php _e("Sorry, but something is missing. Please try again!", "serena"); ?></p>
+					</section>
+					<footer class="article-footer">
+					</footer>
+				</article>	
 
-						get_template_part( 'template-parts/content' );
+			<?php endif; ?>
 
-					endwhile;
-					?>	
-						
-				        <nav class="wp-prev-next">
-				            <ul>
-				    	        <li class="prev-link"><?php next_posts_link(__('&laquo; Older', "serena")) ?></li>
-				    	        <li class="next-link"><?php previous_posts_link(__('Newer &raquo;', "serena")) ?></li>
-				            </ul>
-				        </nav>					
+		</div> <!-- end #main -->				    
 
-				    <?php else : ?>
-					
-				        <article id="post-not-found" class="hentry clearfix">
-				            <header class="article-header">
-				        	    <h2 class="h1"><?php _e("Article Missing", "serena"); ?></h2>
-				        	</header>
-				            <section class="entry-content">
-				        	    <p><?php _e("Sorry, but something is missing. Please try again!", "serena"); ?></p>
-				        	</section>
-				        	<footer class="article-footer">
-				        	</footer>
-				        </article>	
+		<?php get_sidebar(); ?>
 
-				    <?php endif; ?>
-			
-			    </div> <!-- end #main -->				    
-    
-			   <?php get_sidebar(); ?>
-    
-			</div> <!-- end #content -->
+	</div> <!-- end #content -->
 
 <?php get_footer(); ?>
